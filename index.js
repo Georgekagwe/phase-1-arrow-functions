5b40591465c306005bb1f2b11442dd9b707ebbc3
George <george.kagwe@student.moringaschool.com>
2025-03-11 09:00:13 +0300

    arrow functions lab completed

---
+++ index.js
@@ -0,0 +1,12 @@
+let a=2000
+let b=100
+
+function divide(a,b) {
+    return a/b;
+    
+}
+
+const square = (parameter1) => parameter1**2
+
+const add = (parameter1,parameter2) => parameter1+parameter2
+
